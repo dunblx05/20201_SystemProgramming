e0b671691a880f93041713b4a0d104be805229e2
NaHyunJun <nhj2927@naver.com>
2020-12-19 20:14:54 +0900

    add : words

---
+++ hangman/words.h
@@ -0,0 +1,47 @@
+char *animals[] = {
+"cat","frog","chicken","turtle","crab","rabbit",
+"shark","giraffe","cow","horse",
+"bull","pig","sheep","snake",
+"panda","fish","penguin","tiger","duck"
+};
+char *food[] = {
+"chicken","noodles","baguette","apple",
+"sandwich","muffin","oranges","candy","ketchup",
+"shrimp","coconuts","bananas","fish","pie","wine","eggs","lamb",
+"wine","lettuce"
+};
+char *clothes[] = {
+"Tshirt","suit","belt","pants","vest","gloves","dress","ring","skirt",
+"earring","shirt","jacket","scarf","shorts","tie","coat","socks",
+"sweater","shoes"
+};
+char *jobs[] = {
+"policeman","nurse","designer","mechanic","doctor","artist",
+"dancer","cashier","pilot","driver","carpenter",
+"scientist","reporter","musician","lumberjack","teacher","dishwasher","farmer",
+"waiter"
+};
+char *sports[] = {
+"soccer","cycling","wrestling","diving","basketball","baseball",
+"hockey","bowling","swimming","pool","chess","cricket","archery","boxing",
+"golf","javelin","tennis","running","skiing"
+};
+char *body[] = {
+"eyes","teeth","toes","head","eyebrow","ears","hair","shoulder","tongue","bones","hand",
+"finger","knee","moustache","ankle","nose","leg","thumb","neck"
+};
+char *kitchen[] = {
+"cup","kettle","corkscrew","bowl","toaster","mixer","glass","jug","microwave","dishcloth",
+"grater","knife","saucepan","teapot","cleaver","fork","chopsticks","plate","bottle"
+};
+char *transports[] = {
+"bike","car","van","scooter","rocket","bus","truck","ambulance","skateboard","moped","plane",
+"train","helicopter","escalator","tractor","motorbike","boat","elevator","limo"
+};
+char *things[] = {
+"candle","magnet","satchel","atm","backpack","dice","padlock","letter","microscope","ballon",
+"globe","can","sword","coins","box","luggage","axe","bolt","ball"
+};
+
+char** themes[] = {animals,food,clothes,jobs,sports,body,kitchen,transports,things};
+
