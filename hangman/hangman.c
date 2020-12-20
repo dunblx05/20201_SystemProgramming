7e7a573f18929456e3c2867914a1466cc751dd5c
NaHyunJun <nhj2927@naver.com>
2020-12-20 15:43:00 +0900

    add : game function, timer

--- hangman/hangman.c
+++ hangman/hangman.c
@@ -1,550 +1,255 @@
-void drawStart()
-{
-	if (has_colors())
-	{
-		start_color();
+#include <stdio.h>
+#include <curses.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include "words.h"
 
-		init_pair(1, COLOR_WHITE, COLOR_RED);
-		init_pair(4, COLOR_WHITE, COLOR_BLUE);
 
+char *themeName[] = {"animals","food","clothes","jobs","sports","body","kitchen","transports","things"};
+char *words;
+int timeOver = 0;
+int num = 10;
+
+void printV();
+void printH();
+void drawBorder();
+void drawStart();
+void drawSelect();
+void drawGameOver();
+void drawNice();
+void drawHangMan(int);
+
+//gameover signal handler
+void GameOver(int signum){
+	clear();
+	drawBorder();
+	drawGameOver();
+	move(LINES-1,COLS-1);
+}
+
+//nice signal handler
+void Nice(int signum){
+	clear();
+	drawBorder();
+	drawNice();
+	move(LINES-1,COLS-1);
+}
+
+int set_ticker(int n_msecs){
+	struct itimerval new_timeset;
+	long n_sec, n_usecs;
+	
+	n_sec = n_msecs/1000;
+	n_usecs = (n_msecs%1000)*1000L;
+	new_timeset.it_interval.tv_sec = n_sec;
+	new_timeset.it_interval.tv_usec = n_usecs;
+	new_timeset.it_value.tv_sec = n_sec;
+	new_timeset.it_value.tv_usec = n_usecs;
+	
+	return setitimer(ITIMER_REAL, &new_timeset, NULL);
+}
+//timeover signal handler
+void countdown(int signum){
+	//print time
+	move(4,71);
+	attroff(COLOR_PAIR(1));
+	printw("%d",num);
+	if(num >= 10)
+		move(4,73);
+	else{
+		if(num == 9){
+			move(4,73);
+			addch(' ');
+		}
+		move(4,72);
 	}
-	attron(COLOR_PAIR(1));
-
-	//H
-	move(5, 15);
-	addstr(" ");
-	move(6, 15);
-	addstr(" ");
-	move(7, 15);
-	addstr(" ");
-	move(8, 15);
-	addstr(" ");
-	move(9, 15);
-	addstr(" ");
-
-	move(5, 14);
-	addstr(" ");
-	move(6, 14);
-	addstr(" ");
-	move(7, 14);
-	addstr(" ");
-	move(8, 14);
-	addstr(" ");
-	move(9, 14);
-	addstr(" ");
-
-	move(7, 16);
-	addstr(" ");
-	move(7, 17);
-	addstr(" ");
-	move(7, 18);
-	addstr(" ");
-
-	move(5, 19);
-	addstr(" ");
-	move(6, 19);
-	addstr(" ");
-	move(7, 19);
-	addstr(" ");
-	move(8, 19);
-	addstr(" ");
-	move(9, 19);
-	addstr(" ");
-
-	move(5, 20);
-	addstr(" ");
-	move(6, 20);
-	addstr(" ");
-	move(7, 20);
-	addstr(" ");
-	move(8, 20);
-	addstr(" ");
-	move(9, 20);
-	addstr(" ");
-
-	//A
-	move(5, 22);
-	addstr(" ");
-	move(6, 22);
-	addstr(" ");
-	move(7, 22);
-	addstr(" ");
-	move(8, 22);
-	addstr(" ");
-	move(9, 22);
-	addstr(" ");
-	move(5, 23);
-	addstr(" ");
-	move(6, 23);
-	addstr(" ");
-	move(7, 23);
-	addstr(" ");
-	move(8, 23);
-	addstr(" ");
-	move(9, 23);
-	addstr(" ");
-
-	move(5, 24);
-	addstr(" ");
-	move(5, 25);
-	addstr(" ");
-	move(5, 26);
-	addstr(" ");
-	move(7, 24);
-	addstr(" ");
-	move(7, 25);
-	addstr(" ");
-	move(7, 26);
-	addstr(" ");
-
-	move(5, 27);
-	addstr(" ");
-	move(6, 27);
-	addstr(" ");
-	move(7, 27);
-	addstr(" ");
-	move(8, 27);
-	addstr(" ");
-	move(9, 27);
-	addstr(" ");
-	move(5, 28);
-	addstr(" ");
-	move(6, 28);
-	addstr(" ");
-	move(7, 28);
-	addstr(" ");
-	move(8, 28);
-	addstr(" ");
-	move(9, 28);
-	addstr(" ");
-
-	//N
-	move(5, 30);
-	addstr(" ");
-	move(6, 30);
-	addstr(" ");
-	move(7, 30);
-	addstr(" ");
-	move(8, 30);
-	addstr(" ");
-	move(9, 30);
-	addstr(" ");
-	move(5, 31);
-	addstr(" ");
-	move(6, 31);
-	addstr(" ");
-	move(7, 31);
-	addstr(" ");
-	move(8, 31);
-	addstr(" ");
-	move(9, 31);
-	addstr(" ");
-
-	move(5, 32);
-	addstr(" ");
-	move(6, 32);
-	addstr(" ");
-	move(6, 33);
-	addstr(" ");
-	move(7, 33);
-	addstr(" ");
-	move(7, 34);
-	addstr(" ");
-	move(8, 34);
-	addstr(" ");
-	move(8, 35);
-	addstr(" ");
-	move(9, 35);
-	addstr(" ");
-	move(9, 36);
-	addstr(" ");
-
-	move(8, 36);
-	addstr(" ");
-	move(7, 36);
-	addstr(" ");
-	move(6, 36);
-	addstr(" ");
-	move(5, 36);
-	addstr(" ");
-	move(9, 37);
-	addstr(" ");
-	move(8, 37);
-	addstr(" ");
-	move(7, 37);
-	addstr(" ");
-	move(6, 37);
-	addstr(" ");
-	move(5, 37);
-	addstr(" ");
-
-	//G
-	move(5, 39);
-	addstr(" ");
-	move(6, 39);
-	addstr(" ");
-	move(7, 39);
-	addstr(" ");
-	move(8, 39);
-	addstr(" ");
-	move(9, 39);
-	addstr(" ");
-	move(5, 40);
-	addstr(" ");
-	move(6, 40);
-	addstr(" ");
-	move(7, 40);
-	addstr(" ");
-	move(8, 40);
-	addstr(" ");
-	move(9, 40);
-	addstr(" ");
-
-	move(5, 41);
-	addstr(" ");
-	move(5, 42);
-	addstr(" ");
-	move(5, 43);
-	addstr(" ");
-	move(5, 44);
-	addstr(" ");
-	move(9, 41);
-	addstr(" ");
-	move(9, 42);
-	addstr(" ");
-	move(9, 43);
-	addstr(" ");
-	move(9, 44);
-	addstr(" ");
-	move(7, 43);
-	addstr(" ");
-	move(7, 44);
-	addstr(" ");
-
-	move(5, 45);
-	addstr(" ");
-	move(7, 45);
-	addstr(" ");
-	move(8, 45);
-	addstr(" ");
-	move(9, 45);
-	addstr(" ");
-	move(5, 46);
-	addstr(" ");
-	move(7, 46);
-	addstr(" ");
-	move(8, 46);
-	addstr(" ");
-	move(9, 46);
-	addstr(" ");
-
-	attron(COLOR_PAIR(2));
-
-	//M
-	move(5, 48);
-	addstr(" ");
-	move(6, 48);
-	addstr(" ");
-	move(7, 48);
-	addstr(" ");
-	move(8, 48);
-	addstr(" ");
-	move(9, 48);
-	addstr(" ");
-	move(5, 49);
-	addstr(" ");
-	move(6, 49);
-	addstr(" ");
-	move(7, 49);
-	addstr(" ");
-	move(8, 49);
-	addstr(" ");
-	move(9, 49);
-	addstr(" ");
-
-	move(5, 50);
-	addstr(" ");
-	move(5, 51);
-	addstr(" ");
-	move(5, 52);
-	addstr(" ");
-	move(5, 53);
-	addstr(" ");
-	move(5, 54);
-	addstr(" ");
-	move(5, 55);
-	addstr(" ");
-
-	move(6, 52);
-	addstr(" ");
-	move(6, 53);
-	addstr(" ");
-	move(7, 52);
-	addstr(" ");
-	move(7, 53);
-	addstr(" ");
-	move(8, 52);
-	addstr(" ");
-	move(8, 53);
-	addstr(" ");
-	move(9, 52);
-	addstr(" ");
-	move(9, 53);
-	addstr(" ");
-
-
-	move(5, 56);
-	addstr(" ");
-	move(6, 56);
-	addstr(" ");
-	move(7, 56);
-	addstr(" ");
-	move(8, 56);
-	addstr(" ");
-	move(9, 56);
-	addstr(" ");
-	move(5, 57);
-	addstr(" ");
-	move(6, 57);
-	addstr(" ");
-	move(7, 57);
-	addstr(" ");
-	move(8, 57);
-	addstr(" ");
-	move(9, 57);
-	addstr(" ");
-
-	//A
-	move(5, 59);
-	addstr(" ");
-	move(6, 59);
-	addstr(" ");
-	move(7, 59);
-	addstr(" ");
-	move(8, 59);
-	addstr(" ");
-	move(9, 59);
-	addstr(" ");
-	move(5, 60);
-	addstr(" ");
-	move(6, 60);
-	addstr(" ");
-	move(7, 60);
-	addstr(" ");
-	move(8, 60);
-	addstr(" ");
-	move(9, 60);
-	addstr(" ");
-
-	move(5, 61);
-	addstr(" ");
-	move(5, 62);
-	addstr(" ");
-	move(5, 63);
-	addstr(" ");
-	move(7, 61);
-	addstr(" ");
-	move(7, 62);
-	addstr(" ");
-	move(7, 63);
-	addstr(" ");
-
-	move(5, 64);
-	addstr(" ");
-	move(6, 64);
-	addstr(" ");
-	move(7, 64);
-	addstr(" ");
-	move(8, 64);
-	addstr(" ");
-	move(9, 64);
-	addstr(" ");
-	move(5, 65);
-	addstr(" ");
-	move(6, 65);
-	addstr(" ");
-	move(7, 65);
-	addstr(" ");
-	move(8, 65);
-	addstr(" ");
-	move(9, 65);
-	addstr(" ");
-
-	//N
-	move(5, 67);
-	addstr(" ");
-	move(6, 67);
-	addstr(" ");
-	move(7, 67);
-	addstr(" ");
-	move(8, 67);
-	addstr(" ");
-	move(9, 67);
-	addstr(" ");
-	move(5, 68);
-	addstr(" ");
-	move(6, 68);
-	addstr(" ");
-	move(7, 68);
-	addstr(" ");
-	move(8, 68);
-	addstr(" ");
-	move(9, 68);
-	addstr(" ");
-
-	move(5, 69);
-	addstr(" ");
-	move(6, 69);
-	addstr(" ");
-	move(6, 70);
-	addstr(" ");
-	move(7, 70);
-	addstr(" ");
-	move(7, 71);
-	addstr(" ");
-	move(8, 71);
-	addstr(" ");
-	move(8, 72);
-	addstr(" ");
-	move(9, 72);
-	addstr(" ");
-	move(9, 73);
-	addstr(" ");
-
-	move(8, 73);
-	addstr(" ");
-	move(7, 73);
-	addstr(" ");
-	move(6, 73);
-	addstr(" ");
-	move(5, 73);
-	addstr(" ");
-	move(9, 74);
-	addstr(" ");
-	move(8, 74);
-	addstr(" ");
-	move(7, 74);
-	addstr(" ");
-	move(6, 74);
-	addstr(" ");
-	move(5, 74);
-	addstr(" ");
-
-	attroff(COLOR_PAIR(2));
-	move(14, 38);
-	addstr("1.start");
-	move(15, 38);
-	addstr("2.exit");
+	addch('s');
+	move(LINES-1,COLS-1);
 	refresh();
-}
-
-void printV(int x, int y, int n)
-{
-	for(int i = 0; i < n; i++ )
-	{
-		move(x++,y);
-		printw(" ");
+	num--;
+	//timeover
+	if(num==-1){
+		timeOver = 1;
+		attroff(COLOR_PAIR(1));
+		move(16,55);
+		for(int i=0;i<strlen(words);i++){
+			printw("%c ",words[i]);
+		}
+		move(LINES-1,COLS-1);
+		refresh();
 	}
-
-}
-
-void printH(int x, int y, int n)
-{
-	for(int i = 0; i < n; i++)
-	{
-		move(x,y++);
-		printw(" ");
+	//gameover
+	if(num==-2){
+		clear();
+		drawBorder();
+		drawGameOver();
+		move(LINES-1,COLS-1);
+		refresh();
+		set_ticker(0);
 	}
 }
+//hangman game
+void Game(char input){
+	char alpha;
+	int index;
+	int len;
+	int count = 0;
+	int correct = 0;
+	int correctLen = 0;
+	int startX = 55,startY = 16;
+	int repeat[26] = {0,};
+	
+	//set timeover signal handler
+   	signal(SIGALRM,countdown);
+   	set_ticker(1000);
 
-void drawBorder(){
+	srand(time(NULL));
+	index = rand()%19;
+	words = themes[input-'1'][index];
+	len = strlen(words);
 	if(has_colors())
 	{
 		start_color();
 
 		init_pair(1,COLOR_WHITE,COLOR_WHITE);
-
+		init_pair(2,COLOR_WHITE,COLOR_RED);
 	}
-	attron(COLOR_PAIR(1));	
-
-	printV(2,5,20);
-	printV(2,6,20);
-	printV(2,82,20);
-	printV(2,83,20);
-	printH(2,7,75);
-	printH(21,7,75);
+	attron(COLOR_PAIR(1));
 	
-	refresh();
-}
-
-void drawHangMan(){
-	//head
-	printH(8,30,10);
-	printH(11,30,10);
-	printV(9,30,2);
-	printV(9,31,2);
-	printV(9,38,2);
-	printV(9,39,2);
-	
-	//body
-	printV(12,34,4);
-	printV(12,35,4);
-	
-	//left-hand
-	printH(13,27,7);
-	//right-hand
-	printH(13,36,7);
-	//left-foot
-	printH(16,33,2);
-	printH(17,32,2);
-	printH(18,31,2);
-	printH(19,30,2);
-	//right-foot
-	printH(16,35,2);
-	printH(17,36,2);
-	printH(18,37,2);
-	printH(19,38,2);
-	
-}
-
-void drawGame(){
-	if(has_colors())
-	{
-		start_color();
-
-		init_pair(1,COLOR_WHITE,COLOR_RED);
-		init_pair(2,COLOR_WHITE,COLOR_WHITE);
-
-	}
-	attron(COLOR_PAIR(2));
-		
 	printV(5,13,16);
 	printV(5,14,16);
 	printV(5,34,3);
 	printV(5,35,3);
 	printH(5,15,20);
 	
-	attron(COLOR_PAIR(1));
+	//print UI
+	attron(COLOR_PAIR(2));
 	move(6,53);
-	addstr("Theme : Animal");
-	attroff(COLOR_PAIR(2));
+	printw("Theme : %s",themeName[input-'1']);
+	attroff(COLOR_PAIR(1));
 	move(12,55);
 	addstr("Press key");
-	move(16,55);
-	addstr("a b c d e");
 	move(17,55);
-	addstr("- - - - - ");
+	for(int i=0;i<len;i++){
+		addstr("- ");
+	}
+	move(LINES-1,COLS-1);
 	
-	drawHangMan();	
+	//draw hangman
+	while(1){
+		drawHangMan(count);
+		//game over
+		if(count == 6)
+			break;
+
+		move(LINES-1,COLS-1);
+		while(1){
+			if(timeOver == 1){
+				break;
+			}
+			alpha = getch();
+			if(alpha >= 'a' && alpha <= 'z'){
+				if(repeat[alpha-'a'] == 0)
+					break;
+			}
+		}
+		if(timeOver == 1){
+			break;
+		}
+		for(int i=0;i<len;i++){
+			//correct alphabet
+			if(alpha==words[i]){
+				attroff(COLOR_PAIR);
+				move(startY,startX+2*i);
+				addch(alpha);
+				move(LINES-1,COLS-1);
+				correctLen++;
+				correct = 1;
+				repeat[alpha-'a'] = 1;
+			}
+		}
+		//wrong alphabet
+		if(correct == 0){
+			count++;
+		}
+		else{
+			correct = 0;
+		}
+		//correct word
+		if(correctLen==len)
+			break;
+	}
+	
+	if(timeOver == 1){
+		return;
+	}
+	//print word
+	attroff(COLOR_PAIR(1));
+	move(16,55);
+	for(int i=0;i<strlen(words);i++){
+		printw("%c ",words[i]);
+	}
+	move(LINES-1,COLS-1);
+	refresh();
+	
+	//game over
+	if(count == 6){
+		signal(SIGALRM,GameOver);
+		alarm(1);
+		pause();
+			
+	}
+	//correct answer
+	else{
+		signal(SIGALRM,Nice);
+		alarm(1);
+		pause();
+		
+	}
 }
 
 void main(){
 	initscr();
+	noecho();
 	clear();
-	drawBorder();
-	drawGame();
-	move(LINES-1,COLS-1);
-	getch();
-	endwin();
 	
+	int input;
+	//start game
+	while(1){
+		drawBorder();
+		drawStart();	
+		move(LINES-1,COLS-1);
+		input = getch();
+		
+		if(input == '1'){
+			clear();
+			drawBorder();
+			drawSelect();
+			move(LINES-1,COLS-1);
+			
+			while(1){
+				input = getch();
+				if(input >= '1' && input <='9'){
+					break;
+				}
+			}		
+			clear();
+			drawBorder();
+			Game(input);
+			if(timeOver==0)
+				getch();
+			timeOver = 0;
+			num = 10;
+			clear();
+		}
+		else{
+			endwin();
+			break;
+		}
+	}
+		
 }
